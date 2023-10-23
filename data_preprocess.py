d3ceffde05e9c94413e6bc53c6488934498946dc
leo_kuo <leokou42@gmail.com>
2023-10-23 16:15:02 +0800

    data_preprocess modifilize

--- data_preprocess.py
+++ data_preprocess.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 
-folder_name = './viberation_dataset/Learning_set/Bearing1_1/'
-files = [file for file in sorted(os.listdir(folder_name)) if 'acc' in file]
-# print(len(files))
+# folder_name = './viberation_dataset/Learning_set/Bearing1_1/'
 
-hrz_data_list = []
-for file in files:
-    df_hrz = pd.read_csv(f'{folder_name}/{file}', header=None)
-    df_hrz = df_hrz[4]
-    hrz_data_list.append(df_hrz)
+def hrz_data(folder_name):
+    files = [file for file in sorted(os.listdir(folder_name)) if 'acc' in file]
     
-hrz_data = np.array(hrz_data_list)
-scaler = MinMaxScaler(feature_range=(-1, 1))
-normalized_data = scaler.fit_transform(hrz_data)
+    hrz_data_list = []
+    for file in files:
+        df_hrz = pd.read_csv(f'{folder_name}/{file}', header=None)
+        df_hrz = df_hrz[4]
+        hrz_data_list.append(df_hrz)
+        
+    hrz_data = np.array(hrz_data_list)
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    normalized_data = scaler.fit_transform(hrz_data)
+
+    return normalized_data
