3ed385af11ee7084b88d48768b547b72c8c0d0d1
leo_kuo <leokou42@gmail.com>
2023-11-20 16:44:24 +0800

    train and test modelized

--- src/test.py
+++ src/test.py
@@ -12,38 +12,42 @@ from model import LA_WKN_BiGRU
 work_condition = 1
 batch_size = 32
 
-Test_set = 'F:/git_repo/WKN_SSO/viberation_dataset/Test_set/Bearing1_3'
-# Test_set = "./viberation_dataset/Test_set/Bearing1_3"
-Bearing_name = Test_set.split('/')[-1]
-test_data = CustomDataSet(Test_set, work_condition, mode='test')
-test_loader = DataLoader(test_data, batch_size=batch_size)
+def Test_pipeline(Test_set, batch_size, work_condition, show_pic = False):
+    Bearing_name = Test_set.split('/')[-1]
+    test_data = CustomDataSet(Test_set, work_condition, mode='test')
+    test_loader = DataLoader(test_data, batch_size=batch_size)
+    model = LA_WKN_BiGRU()
 
-model = LA_WKN_BiGRU() 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = LA_WKN_BiGRU().to(device)
-model.load_state_dict(torch.load('your_model.pth'))
-model.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
-print("target: {}".format(Test_set))
-temp_ans = []
-with torch.no_grad():
-    for data in test_loader:
-        data = data.to(device)
-        outputs = model(data)
-        # print("size: {}".format(outputs.size()))
-        # print(outputs)
-        vals = outputs.tolist()
-        for val in vals:
-            temp_ans.append(val)
+    model = LA_WKN_BiGRU().to(device)
+    model.load_state_dict(torch.load('your_model.pth'))
+    model.eval()
 
-print(temp_ans)
+    print("target: {}".format(Test_set))
+    ans = []
+    with torch.no_grad():
+        for data in test_loader:
+            data = data.to(device)
+            outputs = model(data)
+            # print("size: {}".format(outputs.size()))
+            # print(outputs)
+            vals = outputs.tolist()
+            for val in vals:
+                ans.append(val)
+    # print(ans)
 
-# 繪製圖形並儲存
-plt.plot(temp_ans)
-plt.title(Bearing_name)
-plt.ylabel('Health Index')
-plt.xlabel('Time')
-pic_name = 'F:/git_repo/WKN_SSO/result/' + Bearing_name + '.png'
-plt.savefig(pic_name)
-plt.show()
-print("picture saved")
+    # 繪製圖形並儲存
+    plt.plot(ans)
+    plt.title(Bearing_name)
+    plt.ylabel('Health Index')
+    plt.xlabel('Time')
+    pic_name = 'F:/git_repo/WKN_SSO/result/' + Bearing_name + '.png'
+    plt.savefig(pic_name)
+    if show_pic == True:
+        plt.show()
+        
+    print("{} picture saved".format(Bearing_name))
+
+    return ans
--- src/train.py
+++ src/train.py
@@ -8,63 +8,66 @@ from sklearn.model_selection import train_test_split
 from model import LA_WKN_BiGRU
 from dataset_loader import CustomDataSet
 
-# hyperparameter setup
-batch_size = 32
-num_epochs = 50
-learning_rate = 0.001
+# Modelize
+def Train_pipeline(Learning_set, hyper_parameter, work_condition, exp_num):
+    # access to cuda
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
-# setup experiment working condition and dataset location
-work_condition = 1
-Learning_set = 'F:/git_repo/WKN_SSO/viberation_dataset/Learning_set/'
-Test_set = 'F:/git_repo/WKN_SSO/viberation_dataset/Test_set/'
-# Learning_set = "./viberation_dataset/Learning_set/"
-# Test_set = "./viberation_dataset/Test_set/"
+    # hyperparameter setup
+    batch_size = hyper_parameter[0]
+    num_epochs = hyper_parameter[1]
+    learning_rate = hyper_parameter[2]
 
-train_data = CustomDataSet(Learning_set, work_condition, mode='train')
-test_data = CustomDataSet(Test_set, work_condition, mode='test')
+    # setup experiment working condition and dataset location
+    train_data = CustomDataSet(Learning_set, work_condition, mode='train')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+    train_size = int(0.8 * len(train_data))
+    val_size = len(train_data) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(train_data, [train_size, val_size])
 
-train_size = int(0.8 * len(train_data))
-val_size = len(train_data) - train_size
-train_dataset, val_dataset = torch.utils.data.random_split(train_data, [train_size, val_size])
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size)
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size)
-test_loader = DataLoader(test_data, batch_size=batch_size)
+    model = LA_WKN_BiGRU().to(device)
 
-model = LA_WKN_BiGRU().to(device)
+    criterion = nn.MSELoss() 
+    optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
 
-criterion = nn.MSELoss() 
-optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-
-# 訓練模型
-for epoch in range(num_epochs):
-    model.train()
-    for data, labels in train_loader:
-        data, labels = data.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = model(data)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-    # 在驗證集上評估模型
-    model.eval()
-    total_mse = 0
-    num_samples = 0
-    with torch.no_grad():
-        for data, labels in val_loader:
+    # 訓練模型
+    act_MSE = 0
+    for epoch in range(num_epochs):
+        model.train()
+        for data, labels in train_loader:
             data, labels = data.to(device), labels.to(device)
+            optimizer.zero_grad()
             outputs = model(data)
-            mse = criterion(outputs, labels)
-            total_mse += mse.item() * labels.size(0)
-            num_samples += labels.size(0)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-    average_mse = total_mse / num_samples
-    print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss:.4f}, Validation MSE: {average_mse:.4f}')
+        # 在驗證集上評估模型
+        model.eval()
+        total_mse = 0
+        num_samples = 0
+        with torch.no_grad():
+            for data, labels in val_loader:
+                data, labels = data.to(device), labels.to(device)
+                outputs = model(data)
+                mse = criterion(outputs, labels)
+                total_mse += mse.item() * labels.size(0)
+                num_samples += labels.size(0)
 
-# 保存模型
-torch.save(model.state_dict(), 'your_model.pth')
+        average_mse = total_mse / num_samples
+        act_MSE += average_mse
+        print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss:.4f}, Validation MSE: {average_mse:.4f}')
+    
+    act_MSE = act_MSE / epoch
+    print("Process MSE = {}".format(act_MSE))
+
+    # 保存模型
+    model_name = exp_num + '.pth'
+    torch.save(model.state_dict(), model_name)
+    
+    return model_name
 
