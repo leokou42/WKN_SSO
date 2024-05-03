6312bedd5825e74b540a2e5ad59b042dc8432029
leokou42 <leokou42@gmail.com>
2024-05-03 14:48:31 +0800

    SSO exp done

--- src/SSO_aug.py
+++ src/SSO_aug.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
     Validation_set = 'F:/git_repo/WKN_SSO/viberation_dataset/Validation_set/'
     work_condition = [2]
     exp_topic = 'SSO_ML'
-    exp_num = 3
+    exp_num = 5
 
     random_number_range=[(1, 1000),     # learning rate     0
                         (1, 64),        # LA kernel num     1
